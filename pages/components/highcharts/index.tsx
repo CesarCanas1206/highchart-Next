4dc97482a358812028dd6d44c6a024573cdc40b8
Oskar0112 <oskardev0112@gmail.com>
2023-07-25 21:16:56 +0200

    fix:some trivial comment code

--- pages/components/highcharts/index.tsx
+++ pages/components/highcharts/index.tsx
@@ -1,38 +1,8 @@
-// import Head from 'next/head';
+
 import SidebarLayout from '@/layouts/SidebarLayout';
 import fetch from 'isomorphic-fetch';
-// import PageTitle from '@/components/PageTitle';
-// import { useState } from 'react';
 
-// import PageTitleWrapper from '@/components/PageTitleWrapper';
-// import {
-//   Container,
-//   Grid,
-//   Card,
-//   CardHeader,
-//   CardContent,
-//   Divider
-// } from '@mui/material';
-// import Footer from 'src/components/Footer';
 
-// import Box from '@mui/material/Box';
-// import TextField from '@mui/material/TextField';
-// import MenuItem from '@mui/material/MenuItem';
-// import { pink } from '@mui/material/colors';
-// import Checkbox from '@mui/material/Checkbox';
-
-// import Radio from '@mui/material/Radio';
-// import RadioGroup from '@mui/material/RadioGroup';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import FormControl from '@mui/material/FormControl';
-// import FormLabel from '@mui/material/FormLabel';
-
-// import Stack from '@mui/material/Stack';
-// import Slider from '@mui/material/Slider';
-// import VolumeDown from '@mui/icons-material/VolumeDown';
-// import VolumeUp from '@mui/icons-material/VolumeUp';
-
-// import Switch from '@mui/material/Switch';
 import CandlestickChart from "./candlestick";
 
 export async function getServerSideProps() {
